f9de8d1adde5039ce1fa96d2b2437c0249a1fa06
Wizzymotion <admin@wizzymotion.com>
2022-10-16 10:22:39 -0300

    entrega desafio 01

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,29 +1,54 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import '../styles/tasklist.scss'
+import "../styles/tasklist.scss";
 
-import { FiTrash, FiCheckSquare } from 'react-icons/fi'
+import { FiCheckSquare, FiTrash } from "react-icons/fi";
 
+import { v4 as uuidv4 } from "uuid";
 interface Task {
-  id: number;
+  id: string;
   title: string;
   isComplete: boolean;
 }
 
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [newTaskTitle, setNewTaskTitle] = useState("");
 
-  function handleCreateNewTask() {
+  function handleCreateNewTask(title: string) {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+    const newTask = {
+      id: uuidv4(),
+      title: title,
+      isComplete: false,
+    };
+    setTasks([...tasks, newTask]);
+    setNewTaskTitle("");
   }
 
-  function handleToggleTaskCompletion(id: number) {
+  const handleKeyDown = (key: string, title: string) => {
+    if (key === "Enter") {
+      handleCreateNewTask(title);
+    }
+  };
+
+  function handleToggleTaskCompletion(id: string) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const curTasks = tasks.map((item: Task) => {
+      if (item.id == id) {
+        return { ...item, isComplete: !item.isComplete };
+      } else {
+        return item;
+      }
+    });
+    setTasks(curTasks);
   }
 
-  function handleRemoveTask(id: number) {
+  function handleRemoveTask(id: string) {
     // Remova uma task da listagem pelo ID
+    const newTasks = tasks.filter((item) => item.id !== id);
+    setTasks(newTasks);
   }
 
   return (
@@ -32,25 +57,33 @@ export function TaskList() {
         <h2>Minhas tasks</h2>
 
         <div className="input-group">
-          <input 
-            type="text" 
-            placeholder="Adicionar novo todo" 
+          <input
+            type="text"
+            placeholder="Adicionar novo todo"
+            onKeyDown={(e) => handleKeyDown(e.key, e.target.value)}
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
-          <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
-            <FiCheckSquare size={16} color="#fff"/>
+          <button
+            type="submit"
+            data-testid="add-task-button"
+            onClick={() => handleCreateNewTask(newTaskTitle)}
+          >
+            <FiCheckSquare size={16} color="#fff" />
           </button>
         </div>
       </header>
 
       <main>
         <ul>
-          {tasks.map(task => (
+          {tasks.map((task) => (
             <li key={task.id}>
-              <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
+              <div
+                className={task.isComplete ? "completed" : ""}
+                data-testid="task"
+              >
                 <label className="checkbox-container">
-                  <input 
+                  <input
                     type="checkbox"
                     readOnly
                     checked={task.isComplete}
@@ -61,14 +94,17 @@ export function TaskList() {
                 <p>{task.title}</p>
               </div>
 
-              <button type="button" data-testid="remove-task-button" onClick={() => handleRemoveTask(task.id)}>
-                <FiTrash size={16}/>
+              <button
+                type="button"
+                data-testid="remove-task-button"
+                onClick={() => handleRemoveTask(task.id)}
+              >
+                <FiTrash size={16} />
               </button>
             </li>
           ))}
-          
         </ul>
       </main>
     </section>
-  )
+  );
 }
